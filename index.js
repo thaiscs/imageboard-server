4786f058a936d1807392c84f79e080de954ab214
thaiscs <thais.advj@gmail.com>
2019-12-06 11:18:20 +0100

    added model

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 4000;
 const db = require("./db");
+const Image = require("./model");
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
