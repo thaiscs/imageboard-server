42e4b7940dd5e77c0679284a9ddbfe83830cb446
thaiscs <thais.advj@gmail.com>
2019-12-06 10:48:19 +0100

    added db connection

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 4000;
+const db = require("./db");
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
